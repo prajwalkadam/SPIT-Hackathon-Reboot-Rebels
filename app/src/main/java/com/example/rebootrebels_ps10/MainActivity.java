250f59a377f4a014c565a145de57c95ceecda9d1
SayuriYuto <dhuriamey02@gmail.com>
2023-02-04 22:15:39 +0530

    Logo and Notifications Done

--- app/src/main/java/com/example/rebootrebels_ps10/MainActivity.java
+++ app/src/main/java/com/example/rebootrebels_ps10/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.application.isradeleon.notify.Notify;
+
 public class MainActivity extends AppCompatActivity {
     private static final int SPLASH_SCREEN = 3000;
     @Override
@@ -17,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         },SPLASH_SCREEN);
+        Notify.build(getApplicationContext())
+                .setTitle("Jill Zhao")
+                .setContent("Hi! So I meet you today?")
+                .setSmallIcon(R.drawable.ic_home)
+                .setLargeIcon("https://images.pexels.com/photos/139829/pexels-photo-139829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=150&w=440")
+                .largeCircularIcon()
+                .setPicture("https://images.pexels.com/photos/1058683/pexels-photo-1058683.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+                .setColor(R.color.black)
+                .show();
     }
 }
