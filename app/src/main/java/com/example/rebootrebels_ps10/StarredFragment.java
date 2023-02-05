73889559a737d8b1cc7f258c1349b9aba283029b
SayuriYuto <dhuriamey02@gmail.com>
2023-02-05 05:41:38 +0530

    earn screen

--- app/src/main/java/com/example/rebootrebels_ps10/StarredFragment.java
+++ app/src/main/java/com/example/rebootrebels_ps10/StarredFragment.java
@@ -15,12 +15,29 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
  * Use the  factory method to
  * create an instance of this fragment.
  */
+
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
 public class StarredFragment extends Fragment {
 
     public StarredFragment() {
-        // Required empty public constructor
     }
 
 
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_search, container, false);
+//    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_starred, container, false);
+        return view;
+    }
 }
